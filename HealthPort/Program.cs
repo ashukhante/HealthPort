27af82010aeecf5972856e18d8ba7214484e2002
Ashwini Khante <ashukhante@gmail.com>
2020-01-18 20:11:54 -0800

    Added Program

--- HealthPort/Program.cs
+++ HealthPort/Program.cs
@@ -6,7 +6,30 @@ namespace HealthPort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var myMemberServices = new MemberServices();
+            myMemberServices.subscribername = "John";
+            myMemberServices.subscriberid = 123456;
+            myMemberServices.primaryaccountholdersname = "Jane";
+            myMemberServices.emailaddress = "janejohn@yahoo.com";
+            myMemberServices.MemberSince = 01/01/2001;
+            myMemberServices.statements = 
+            myMemberServices.Balanceavailableindeductiblle = 12345
+
+            var myMemberservices2 = new MemberServices();
+            myMemberServices.subscribername = "Johny";
+            myMemberServices.subscriberid = 123455;
+            myMemberServices.primaryaccountholdersname = "Janel";
+            myMemberServices.emailaddress = "janeljohny@yahoo.com";
+            myMemberServices.MemberSince = 01 / 01 / 2001;
+            myMemberServices.statements =
+            myMemberServices.Balanceavailableindeductiblle = 12345
+
+
+
+
+
+
+
         }
     }
 }
