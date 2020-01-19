78deb6ac150f827504fff738493b78e2992402f5
ashukhante <59577769+ashukhante@users.noreply.github.com>
2020-01-19 11:46:41 -0800

    Update Program.cs

--- HealthPort/Program.cs
+++ HealthPort/Program.cs
@@ -7,21 +7,21 @@ namespace HealthPort
         static void Main(string[] args)
         {
            var myMemberServices = new MemberServices();
-            myMemberServices.subscribername = "John";
-            myMemberServices.subscriberid = 123456;
-            myMemberServices.primaryaccountholdersname = "Jane";
-            myMemberServices.emailaddress = "janejohn@yahoo.com";
+            myMemberServices.SubscriberName = "John";
+            myMemberServices.SubscriberId = 123456;
+            myMemberServices.PrimaryAccountholdersName = "Jane";
+            myMemberServices.EmailAddress = "janejohn@yahoo.com";
             myMemberServices.MemberSince = new DateTime(2001, 1, 1);
-            myMemberServices.statements = new DateTime(2001, 1, 1);
+            myMemberServices.Statements = new DateTime(2001, 1, 1);
             myMemberServices.BalanceAvailableIndeductible = 1234;
 
             var myMemberservices2 = new MemberServices();
-            myMemberServices.subscribername = "Johny";
-            myMemberServices.subscriberid = 123455;
-            myMemberServices.primaryaccountholdersname = "Janel";
-            myMemberServices.emailaddress = "janeljohny@yahoo.com";
+            myMemberServices.SubscriberName = "Johny";
+            myMemberServices.SubscriberId = 123455;
+            myMemberServices.PrimaryAccountholdersName = "Janel";
+            myMemberServices.EmailAddress = "janeljohny@yahoo.com";
             myMemberServices.MemberSince = new DateTime(2001, 1, 1);
-            myMemberServices.statements = new DateTime(2001, 1, 1);
+            myMemberServices.Statements = new DateTime(2001, 1, 1);
             myMemberServices.BalanceAvailableIndeductible = 12345;
         }
     }
