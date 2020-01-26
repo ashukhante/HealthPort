4b90317223f38f1070599ec6d197571e83ae6972
ashukhante <59577769+ashukhante@users.noreply.github.com>
2020-01-26 11:28:20 -0800

    Update Program.cs

--- HealthPort/Program.cs
+++ HealthPort/Program.cs
@@ -8,21 +8,17 @@ namespace HealthPort
         {
            var myMemberServices = new MemberServices();
             myMemberServices.SubscriberName = "John";
-            myMemberServices.SubscriberId = 123456;
+            //myMemberServices.SubscriberId = 123456;
             myMemberServices.PrimaryAccountholdersName = "Jane";
             myMemberServices.EmailAddress = "janejohn@yahoo.com";
-            myMemberServices.MemberSince = new DateTime(2001, 1, 1);
-            myMemberServices.Statements = new DateTime(2001, 1, 1);
-            myMemberServices.BalanceAvailableIndeductible = 1234;
+            //myMemberServices.MemberSince = new DateTime(2001, 1, 1);
+            //myMemberServices.Statements = new DateTime(2001, 1, 1);
+            //myMemberServices.BalanceAvailableInDeductible = 1234;
+            myMemberServices.AddFundstobalace(1000);
+            Console.WriteLine($"SubscriberName: {myMemberServices.SubscriberName},SubscriberId: {myMemberServices.SubscriberId}, PrimaryAccountholdersName: {myMemberServices.PrimaryAccountholdersName}, EmailAddress: {myMemberServices.EmailAddress},MemberSince.{myMemberServices.MemberSince}, Statements.{myMemberServices.Statements},BalanceAvailableInDeductible.{myMemberServices.BalanceAvailableInDeductible}");
 
-            var myMemberservices2 = new MemberServices();
-            myMemberServices.SubscriberName = "Johny";
-            myMemberServices.SubscriberId = 123455;
-            myMemberServices.PrimaryAccountholdersName = "Janel";
-            myMemberServices.EmailAddress = "janeljohny@yahoo.com";
-            myMemberServices.MemberSince = new DateTime(2001, 1, 1);
-            myMemberServices.Statements = new DateTime(2001, 1, 1);
-            myMemberServices.BalanceAvailableIndeductible = 12345;
+           var myMemberservices2 = new MemberServices();
+            Console.WriteLine($"SubscriberName: {myMemberservices2.SubscriberName},SubscriberId: {myMemberservices2.SubscriberId}, PrimaryAccountholdersName: {myMemberservices2.PrimaryAccountholdersName}, EmailAddress: {myMemberservices2.EmailAddress},MemberSince.{myMemberservices2.MemberSince}, Statements.{myMemberservices2.Statements},BalanceAvailableInDeductible.{myMemberservices2.BalanceAvailableInDeductible}");
         }
     }
 }
