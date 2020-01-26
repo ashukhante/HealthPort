610ea519bbb0ef4e90f70bec8438e97e4fbcd7c6
ashukhante <59577769+ashukhante@users.noreply.github.com>
2020-01-26 11:27:30 -0800

    Update MemberServices.cs

--- HealthPort/MemberServices.cs
+++ HealthPort/MemberServices.cs
@@ -10,6 +10,7 @@ namespace HealthPort
 /// </summary>
     class MemberServices
     {
+        private static int lastSubscriberId = 0;
         #region properties
         /// <summary>
         /// Unique Insurnace Information for the subscriber
@@ -17,26 +18,37 @@ namespace HealthPort
         public int SubscriberId { get; set; }
         public string SubscriberName { get; set; }
         public string PrimaryAccountholdersName { get; set; }
-        public DateTime MemberSince { get; set; }
+        public DateTime MemberSince { get; private set; }
         public string EmailAddress { get; set; }
-        public float BalanceAvailableIndeductible { get; set; }
-        public DateTime Statements { get; set; }
+        public float BalanceAvailableInDeductible { get; private set; }
+        public DateTime Statements { get; private set; }
         #endregion
+
+        #region  Constructor
+        public MemberServices()
+        {
+            //lastSubscriberId += 1;
+            SubscriberId = ++lastSubscriberId;
+            MemberSince = DateTime.UtcNow;
+        }
+
+        #endregion
+
 
         #region  Methods
         /// <summary>
         /// Add funds to the balance
         /// </summary>
         /// <param name="amount">Amount to add towards existing funds</param>
-        public void AddFundsobalace(float amount)
+        public void AddFundstobalace(float amount)
         {
-            BalanceAvailableIndeductible += amount;
+            BalanceAvailableInDeductible += amount;
         }
 
         public float WithdrawToPayTheBill(float amount)
         {
-            BalanceAvailableIndeductible -= amount;
-            return BalanceAvailableIndeductible;
+            BalanceAvailableInDeductible -= amount;
+            return BalanceAvailableInDeductible;
         }
         #endregion
     }
