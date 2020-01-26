0ff276b2b1ced9b0518980f8979d2985bbd3921d
ashukhante <59577769+ashukhante@users.noreply.github.com>
2020-01-26 11:25:56 -0800

    Update Insurance.cs

--- HealthPort/Insurance.cs
+++ HealthPort/Insurance.cs
@@ -1,1 +1,26 @@
 
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthPort
+{
+    class Insurance
+    {
+        public MemberServices CreateMemberServices(string subscriberName,
+            string primaryAccountholdersName, string emailAddress)
+        {
+
+            //Object initialization
+            var memberservices = new MemberServices
+            {
+                SubscriberName = subscriberName,
+                PrimaryAccountholdersName = primaryAccountholdersName,
+                EmailAddress = emailAddress
+            };
+
+            //Save upsert and get modified MemberServices
+            return memberservices;
+        }
+    }
+}
