8dc5625aa60b28f372b904e49c8a8c611550a4c2
ashukhante <59577769+ashukhante@users.noreply.github.com>
2020-01-26 11:39:13 -0800

    Update Program.cs

--- HealthPort/Program.cs
+++ HealthPort/Program.cs
@@ -10,7 +10,8 @@ namespace HealthPort
                 "y", "x@y.z");
             Console.WriteLine($"SubscriberName: {myMemberServices.SubscriberName},SubscriberId: {myMemberServices.SubscriberId}, PrimaryAccountholdersName: {myMemberServices.PrimaryAccountholdersName}, EmailAddress: {myMemberServices.EmailAddress},MemberSince.{myMemberServices.MemberSince}, Statements.{myMemberServices.Statements},BalanceAvailableInDeductible.{myMemberServices.BalanceAvailableInDeductible}");
 
-           var myMemberservices2 = new MemberServices();
+           var myMemberservices2 = Insurance.CreateMemberServices("a",
+                "b", "a@b.c");
             Console.WriteLine($"SubscriberName: {myMemberservices2.SubscriberName},SubscriberId: {myMemberservices2.SubscriberId}, PrimaryAccountholdersName: {myMemberservices2.PrimaryAccountholdersName}, EmailAddress: {myMemberservices2.EmailAddress},MemberSince.{myMemberservices2.MemberSince}, Statements.{myMemberservices2.Statements},BalanceAvailableInDeductible.{myMemberservices2.BalanceAvailableInDeductible}");
         }
     }
